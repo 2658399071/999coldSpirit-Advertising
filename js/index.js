3c174a765e4e8f30f13c5bb9cc392147c65702ab
2658399071 <2658399071@qq.com>
2024-05-31 16:47:17 +0800

    first
    代码

---
+++ js/index.js
@@ -0,0 +1,155 @@
+var ad = document.querySelector('.max-one');
+setTimeout(function () {
+    ad.style.display = 'none';
+}, 4000);
+setTimeout(function () {
+  bjTwo.classList.add('top-bj-twot');
+}, 3000);
+var ac = document.querySelector('.logo');
+setTimeout(function () {
+    ac.style.display = 'none';
+}, 3800);
+function bottomone() {
+    var image = document.getElementById('max-two');
+    image.style.display = 'none';
+
+}
+const But = document.getElementById("But")
+const two_page = document.getElementById("max-two")
+But.onclick=function (){
+    two_page.style.display = "none";
+    console.log("2")
+}
+const Buttwo = document.getElementById("Buttwo")
+Buttwo.onclick=function (){
+    maxFive.style.display = "none";
+    console.log("2")
+}
+var bjTwo = document.getElementsByClassName("top-bj-two")[0];
+
+var twoBottom = document.getElementsByClassName("two-bottom")[0];
+var maxTwo = document.getElementsByClassName("max-two")[0];
+var oneBj = document.getElementsByClassName("top-one-bj")[0];
+var ganTan = document.getElementsByClassName("gantan")[0];
+var imgOne = document.getElementsByClassName("two-one-img")[0];
+var Ten = document.getElementsByClassName("ten")[0];
+var imgTwo = document.getElementsByClassName("two-two-img")[0];
+var wzOne = document.getElementsByClassName("wz-one")[0];
+var imgThree= document.getElementsByClassName("top-three-img")[0];
+var tjOne= document.getElementsByClassName("tj-one")[0];
+var wzTwo= document.getElementsByClassName("wz-two")[0];
+var tjTwo= document.getElementsByClassName("tj-two")[0];
+var tjThree= document.getElementsByClassName("tj-three")[0];
+var gantanOne= document.getElementsByClassName("gantan-one")[0];
+var gantanTwo= document.getElementsByClassName("gantan-two")[0];
+var maxThree= document.getElementsByClassName("max-three")[0];
+var imgFour= document.getElementsByClassName("top-four-img")[0];
+var wzThree= document.getElementsByClassName("four-wz-one")[0];
+var txOne= document.getElementsByClassName("four-tx-one")[0];
+var wzFour= document.getElementsByClassName("four-wz-two")[0];
+var txTwo= document.getElementsByClassName("four-tx-three")[0];
+var imgFive= document.getElementsByClassName("bottom-four-img")[0];
+var txThree= document.getElementsByClassName("four-tx-two")[0];
+var wzFive= document.getElementsByClassName("four-wz-three")[0];
+var maxFour= document.getElementsByClassName("max-four")[0];
+var imgSix= document.getElementsByClassName("wz-four-img")[0];
+var imgSeven= document.getElementsByClassName("wz-four-imgtwo")[0];
+var maxFive= document.getElementsByClassName("max-five")[0];
+
+
+twoBottom.onclick = function () {
+    maxTwo.style.display = 'none';
+  
+    oneBj.classList.add('top-one-bjt');
+    setTimeout(() => {
+        ganTan.classList.add('gantant');
+    
+      }, 2000);
+      setTimeout(() => {
+        imgOne.classList.add('two-one-imgt');
+    
+      }, 4000);
+      setTimeout(() => {
+        Ten.classList.add('tent');
+    
+      }, 6000);
+      setTimeout(() => {
+        imgTwo.classList.add('two-two-imgt');
+    
+      }, 8000);
+      setTimeout(() => {
+        wzOne.classList.add('wz-onet');
+    
+      }, 10000);
+      setTimeout(() => {
+        imgThree.classList.add('top-three-imgt');
+    
+      }, 12000);
+      
+      setTimeout(() => {
+        tjOne.classList.add('tj-onet');
+    
+      }, 14000);
+      setTimeout(() => {
+        wzTwo.classList.add('wz-twot');
+    
+      }, 16000);
+      setTimeout(() => {
+        tjTwo.classList.add('tj-twot');
+        tjThree.classList.add('tj-threet');
+      }, 18000);
+      setTimeout(() => {
+        gantanOne.classList.add('gantan-onet');
+        gantanTwo.classList.add('gantan-onet');
+      }, 20000);
+      setTimeout(() => {
+        maxThree.style.display='none';
+      }, 23000);
+      setTimeout(() => {
+        imgFour.classList.add('top-four-imgt');
+      }, 25000);
+      setTimeout(() => {
+        imgFive.classList.add('bottom-four-imgt');
+      }, 27000);
+      setTimeout(() => {
+        wzThree.classList.add('four-wz-onet');
+      }, 29000);
+      setTimeout(() => {
+        txOne.classList.add('four-tx-onet');
+      }, 31000);
+      setTimeout(() => {
+        wzFour.classList.add('four-wz-twot');
+      }, 33000);
+      setTimeout(() => {
+        txTwo.classList.add('four-tx-threet');
+      }, 35000);
+      setTimeout(() => {
+        wzFive.classList.add('four-wz-threet');
+      }, 37000);
+      setTimeout(() => {
+        txThree.classList.add('four-tx-twot');
+      }, 39000);
+      setTimeout(() => {
+        imgSix.classList.add('wz-four-imgt');
+        imgSeven.classList.add('wz-four-imgtwot')
+      }, 41000);
+      setTimeout(() => {
+        maxFour.style.display='none';
+      }, 44000);
+      setTimeout(() => {
+        zImg.classList.add('expanded');
+        xImg.classList.add('down');
+        pOne.classList.add('p-onet')
+      }, 46000);
+      setTimeout(() => {
+        pTow.classList.add('p-towt')
+      }, 48000);
+}
+
+var zImg = document.getElementsByClassName("z-img")[0];
+var xImg = document.getElementsByClassName("x-img")[0];
+var pOne = document.getElementsByClassName("p-one")[0];
+var pTow = document.getElementsByClassName("p-tow")[0];
+var yzImg = document.getElementsByClassName("yz-img")[0];
+
+
